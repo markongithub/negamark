0f9cfee691d4098cbf722315862deda711e15292
mark on github <markongithub@users.noreply.github.com>
2017-09-22 14:33:32 +0000

    Paste from SO to make cabal test fail when tests fail. Also a bunch of negamark3 tests.

--- test/NegamarkTest.hs
+++ test/NegamarkTest.hs
@@ -1,4 +1,5 @@
 import Negamark
+import System.Exit
 import TicTacToe
 import Test.HUnit
 import Data.Maybe
@@ -11,6 +12,7 @@ xLetOWin = foldr newTicTacToeStateFromMove newTicTacToeBoard [5,6,7,4,8]
 xIsGonnaWin = foldr newTicTacToeStateFromMove newTicTacToeBoard [2,1,5,0,4]
 xWinsOnLastTurn =
     foldr newTicTacToeStateFromMove newTicTacToeBoard [6,8,7,3,2,1,5,0,4]
+negamark3Bug = foldr newTicTacToeStateFromMove newTicTacToeBoard $ reverse [0,4,1,2]
 
 tests = TestList [
     TestCase (assertEqual "whatever" 
@@ -40,6 +42,8 @@ tests = TestList [
               (firstPass winningBoard)),
     TestCase (assertEqual "14" (Loss 5)
               (resultOutcome (negamark winningBoard 0 (Loss 0) (Win 0)))),
+    TestCase (assertEqual "14.3" (Loss 5)
+              (resultOutcome (negamark3 winningBoard 0 (Loss 0) (Win 0) noMap))),
     TestCase (assertEqual "15" (Heuristic 4 0)
               (firstPass testBoard)),
     TestCase (assertEqual "16" (Win 5)
@@ -50,6 +54,14 @@ tests = TestList [
               (resultOutcome (negamark xIsGonnaWin 4 (Loss 0) (Win 0)))),
     TestCase (assertEqual "whatever" (Loss 9)
               (resultOutcome (negamark xWinsOnLastTurn 0 (Loss 0)(Win 0)))),
+    TestCase (assertEqual "16" (Win 5)
+              (resultOutcome (negamark3 testBoard 6 (Loss 0) (Win 0) noMap))),
+    TestCase (assertEqual "whatever" (Win 5)
+              (resultOutcome (negamark3 testBoard 6 (Loss 0) (Win 0) noMap))),
+    TestCase (assertEqual "whatever" (Loss 7)
+              (resultOutcome (negamark3 xIsGonnaWin 4 (Loss 0) (Win 0) noMap))),
+    TestCase (assertEqual "whatever" (Loss 9)
+              (resultOutcome (negamark3 xWinsOnLastTurn 0 (Loss 0)(Win 0) noMap))),
     TestCase (assertEqual "whatevz" [2, 5, 6, 7, 8] (availableMoves testBoard)),
     TestCase (assertEqual "nsimple on xcanwinnow" (Win 5)
               (resultOutcome (negamarkSimple xCanWinNow 5))),
@@ -60,7 +72,20 @@ tests = TestList [
               (resultOutcome (negamarkSimple xLetOWin 9))),
     TestCase (assertEqual "whatever" X
               (squareState (head (resultMoves (pickMove xCouldLose 5))) 2)),
+    TestCase (assertEqual "the first negamark3 bug" (Stalemate 9)
+              (resultOutcome (negamarkSimple negamark3Bug 6))),
+    TestCase (assertEqual "the first negamark3 bug" (Stalemate 9)
+              (resultOutcome (negamarkSimple3 negamark3Bug 6))),
+    TestCase (assertEqual "slow negamark on TTT" (Stalemate 9)
+              (resultOutcome (negamark newTicTacToeBoard 9 (Loss 0) (Win 0)))),
+    TestCase (assertEqual "slow negamark3 on TTT" (Stalemate 9)
+              (resultOutcome (negamark3 newTicTacToeBoard 9 (Loss 0) (Win 0) noMap))),
     TestCase (assertEqual "I leave this one at the bottom" 1 1)
     ]
 
-main = runTestTT tests
+main = do
+  cs@(Counts _ _ errs fails) <- runTestTT tests
+  putStrLn (showCounts cs)
+  if (errs > 0 || fails > 0)
+    then exitFailure
+    else exitSuccess
